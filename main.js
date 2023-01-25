55a7194a09c41b331a263d6747d9d68780204616
«Pashkov <pashkov921326@gmail.com>
2023-01-25 20:41:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+// ЗАДАЧА 1
+// const a = + prompt("Введите числа а");
+// const b = Number.parseFloat("Введите числа b");
+// const sum = a + b;
+// alert(`Результат сложения чисел ${a} + ${b} = ${sum}`); 
+
+// ЗАДАЧА 2
+// const a = Number.parseInt(prompt("Введите число а"));
+// const b = Number.parseInt(prompt("Введите число а"));
+
+// function sumof(num1, num2) {
+//    return num1 + num2;
+// }
+
+// alert(`Результат сложения чисел ${a} + ${b} = ${sumof(a, b)}`); 
+
+// ЗАДАЧА 3
+// const answer = confirm("Вам хорошо живется?");
+
+// if(answer) { // будет уже тру и фолс  тоже саме что if(answer == true)
+//     alert("Тогда мы идем к вам!");
+// } else {
+//     alert("Ну вы держитесь там!");
+
+// } или можно так:
+// answer ? alert ("Тогда мы идем к вам!") : alert("Ну вы держитесь там!");
+        // Если true                            // Если False
+
+// const product = prompt("какой продукт вам нужен?");
+// switch (product) {
+//     case "Мандарины": 
+//         alert ("Мандарины стоят 100 руб/кг.");
+//         break
+//     case "Бананы": // alert ("Банан и груши стоят 70 руб/кг.") как избаватся от банан и груши и ниже
+//     case "Груши": 
+//     case "Хрен":
+//         alert ("Банан и груши стоят 70 руб/кг.");
+//         break;
+//     default: 
+//          alert("Нет такого продукта");
+// }
+
+// ЗАДАЧА 4
+
+// function hello(usernames) {
+//     console.log(`Привет ${usernames}`);
+// }
+
+// const user = prompt("Введите ваше имя: ")
+// hello(usernames)
